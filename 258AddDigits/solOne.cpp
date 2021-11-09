fa18d61005a000a92408ccf7770aaee08801d51b
Adithya Yerramsetty <adithyay@hotmail.com>
2021-11-08 18:23:54 -0700

    Finished 258AddDigits.

--- 258AddDigits/solOne.cpp
+++ 258AddDigits/solOne.cpp
@@ -1,9 +1,16 @@
-#include <string>
-
 class Solution {
 public:
     int addDigits(int num) {
-      std::string numS;
-      
+      while( ( (int)num / 10 ) != 0) {
+	int sum = 0;
+	while ( num != 0 ) {
+	  sum += num % 10;
+	  num = (int)num/10;
+	}
+
+	num = sum;
+      }
+
+      return num;
     }
 };
